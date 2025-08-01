a99e91edcce1f6645e7c3526f9ade6612dac8f54
Matthew N <mn.gf@outlook.com>
2025-08-01 22:30:56 +0100

    2: confirmed linting works for deployment. This should now work

--- src/components/AccessibilityTest.tsx
+++ src/components/AccessibilityTest.tsx
@@ -1,12 +1,7 @@
 const AccessibilityTest = () => {
     return (
         <div>
-            {/* These will trigger accessibility warnings */}
-            <img src="test.jpg" /> {/* Missing alt text */}
-            <button style={{ color: "#ccc", background: "#ddd" }}>
-                Bad contrast
-            </button>
-            <input type="text" /> {/* Missing label */}
+            <p>Hello World... Accessibly!</p>
         </div>
     );
 };

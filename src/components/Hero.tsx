d0c1830a3396b5dbbd385f8e92af6cb0c6d2684e
Matthew N <mn.gf@outlook.com>
2025-08-27 16:50:20 +0100

    11: add hero-more div

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,31 @@
+import { FaChevronCircleDown } from "react-icons/fa";
+
 export default function Hero() {
     return (
         <section
             id="hero"
             aria-label="Hero section: introduction to Matthew Nolan's Portfolio"
             data-testid="hero"
+            className="hero"
         >
             <div
                 className="container hero__content"
                 data-testid="hero-container"
             >
-                <h1 data-testid="hero-title">Matthew Nolan</h1>
-                <p data-testid="hero-subtitle">Software Developer</p>
+                <h1 data-testid="hero-title" className="hero__content__title">
+                    Matthew Nolan
+                </h1>
+                <p
+                    data-testid="hero-subtitle"
+                    className="hero__content__subtitle"
+                >
+                    Software Developer
+                </p>
+            </div>
+
+            <div data-testid="hero-more" className="hero__more">
+                <p>My work, experience, and more</p>
+                <FaChevronCircleDown />
             </div>
         </section>
     );

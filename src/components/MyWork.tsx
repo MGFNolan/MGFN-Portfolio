5028b118aebe53def3d757d14ae6726c0e616ec5
Matthew N <mn.gf@outlook.com>
2025-09-24 17:34:19 +0100

    16: MyWork and MyWorkCard component styling for desktop

--- src/components/MyWork.tsx
+++ src/components/MyWork.tsx
@@ -3,14 +3,18 @@ import MyWorkCard from "./MyWorkCard";
 
 export default function MyWork() {
     return (
-        <section id="my-work" data-testid="my-work">
+        <section
+            id="my-work"
+            data-testid="my-work"
+            className="container my-work"
+        >
             <h2>My Work</h2>
-            <div className="my-work">
+            <div className="my-work__card-container">
                 {WorkCardContent.map((card) => (
                     <MyWorkCard key={card.id} myWorkCard={card} />
                 ))}
             </div>
-            <button>More...</button>
+            <button className="btn-primary my-work__btn">More...</button>
         </section>
     );
 }

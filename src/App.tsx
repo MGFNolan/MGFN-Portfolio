2ccdb015850dd164f5803bb14407ac7a96dfbc49
Matthew N <mn.gf@outlook.com>
2025-09-24 15:45:09 +0100

    16: add components to the app and update Main to ensure it expects a child prop

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar";
 import Page from "./components/Page";
 import Hero from "./components/Hero";
 import MenuContextProvider from "./context/MobileMenuContext";
+import MyWork from "./components/MyWork";
 
 function App() {
     return (
@@ -15,7 +16,9 @@ function App() {
                     <Hero />
                     <MobileNav />
                 </Header>
-                <Main />
+                <Main>
+                    <MyWork />
+                </Main>
             </Page>
         </MenuContextProvider>
     );
